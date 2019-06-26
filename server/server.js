0b4ec2d5ab0da0c1244064c3bd19ccd5810eacc7
Andrew <andrejmelnyk@gmail.com>
2019-06-26 20:39:51 +0300

    Added connection info console

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname + '/../public');
-
 var app = express();
 const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// app.get('/',(req,res) => {
-//     res.render('index.html');
-// });
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    })
+})
 
-app.listen(port,() => {
+server.listen(port,() => {
     console.log(`Started at port ${port}`);
 });
