7281b675f4f2b6d5341a119825b7d7bb7352100e
Andrew <andrejmelnyk@gmail.com>
2019-06-30 20:10:40 +0300

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -39,11 +39,12 @@ socket.emit('createMessage', {
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
+    var messageTextBox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function () {
-
+        messageTextBox.val('');
     });
 });
 
@@ -53,12 +54,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser')
     };
 
+    locationButton.attr('disabled', 'disabled').text('Sending location ...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('sendCoords', {
             longitude: position.coords.longitude,
             latitude: position.coords.latitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.')
     });
 });
