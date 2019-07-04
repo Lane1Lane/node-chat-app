857cd6b7219700bcd57c48047a24ecddf691e0b9
Andrew <andrejmelnyk@gmail.com>
2019-07-04 23:04:49 +0300

    Added validation for user name and room to be exist

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,16 +10,21 @@ function scrollToBottom() {
     var scrollHeight = messages.prop('scrollHeight');
     var newMessageHeight = newMessage.innerHeight();
     var lastMessageHeight = newMessage.prev().innerHeight();
-    console.log(clientHeight, scrollTop, scrollHeight, newMessageHeight, lastMessageHeight);
     if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
         messages.scrollTop(scrollHeight);
     }
 };
 
 socket.on('connect', function () {
-    console.log('Connected to server'); 
-
-    
+    var params = jQuery.deparam(window.location.search);
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err);
+            window.location.href = '/';
+        } else {
+            console.log('No error');
+        };
+    });
 });
 
 
@@ -61,14 +66,6 @@ socket.on('newLocationMessage', function (message) {
     // a.attr('href', message.url);
     // li.append(a);
     // jQuery('#messages').append(li);
-});
-
-
-socket.emit('createMessage', {
-    from: 'Frank',
-    text: 'Hi'
-}, function (data) {
-    console.log(data);
 });
 
 jQuery('#message-form').on('submit', function (e) {
