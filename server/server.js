84cd125a40c7d1345540d3a220e76796b182a782
andrejmelnyk <andrejmelnyk@gmail.com>
2019-07-23 17:05:33 +0300

    Небольшие изменения

--- server/server.js
+++ server/server.js
@@ -20,9 +20,15 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     socket.on('join', (params, callback) => {
+        params.room = params.room.toLowerCase();
         if (!isRealString(params.name) || !isRealString(params.room)) {
             return callback('Name and room name are required.');
         };
+
+        if (users.getUserList(params.room).filter((user) => user === params.name).length) {
+            return callback(`User ${params.name} already exists`);
+        };
+       
 
         socket.join(params.room);
         users.removeUser(socket.id);
